fd9c049579678aa1a661b6491111058dd0970a12
Coveran <ringo.larios@gmail.com>
2022-09-17 18:56:15 -0500

    Update getGifs.js

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 export const getGifs = async (category) => {
   const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
     category
-  )}&limit=10&api_key=mbC0mquA6jCBQMZrlnhNP2stdwRYRLBK`;
+  )}&limit=10&api_key=`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
